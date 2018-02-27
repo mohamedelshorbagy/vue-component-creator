26361151daf87985d955fbfa48e0d697588ec80a
shorbagy <mohamedelshorbagy96@gmail.com>
2018-02-27 23:55:26 +0200

    Check For Creation of Component

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const os = require("os");
 const createVueComponent = (argvName) => {
 
     let OsType = os.platform();
-        
+
 
     let argvParam = argvName;
     let fileName = argvParam + ".vue";
@@ -27,28 +27,31 @@ const createVueComponent = (argvName) => {
 <style scoped>
 </style>
     `;
-    
+
     let fullDir;
-    if(OsType.indexOf("win") != -1) { // Windows Case
-        fullDir = process.cwd() + "\\" + argvParam + "\\" + fileName; 
+    if (OsType.indexOf("win") != -1) { // Windows Case
+        fullDir = process.cwd() + "\\" + argvParam + "\\" + fileName;
     } else { // Any OS Case
-        fullDir = process.cwd() + "/" + argvParam + "/" + fileName; 
+        fullDir = process.cwd() + "/" + argvParam + "/" + fileName;
     }
-    
-    
-    
-    if(!fs.existsSync(argvParam)) {
+
+
+
+    if (!fs.existsSync(argvParam)) {
         fs.mkdirSync(argvParam);
+        touch(fullDir, function (resp) {
+            fs.writeFile(fullDir, documnetData, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log(argvParam + " Component Was Created!");
+            });
+        })
+    } else {
+        console.log("Component is already Created");
     }
-    
-    touch(fullDir,function(resp) {
-        fs.writeFile(fullDir,documnetData, function(err) {
-            if(err) {
-                return console.log(err);
-            }
-            console.log(argvParam + " Component Was Created!");
-        }); 
-    })
+
+
 }
 
 
